87ac07820a0adcb21afe7c6fe68665df5d61ba9b
鬼王 <THRAEX@aliyun.com>
2020-12-03 18:38:49 +0800

    style(pagation-table): remove thx-widget

--- packages/pagination-table/src/pagination.vue
+++ packages/pagination-table/src/pagination.vue
@@ -1,25 +1,23 @@
 <template>
-  <div class="thx-widget">
-    <el-row type="flex" align="middle">
-      <el-col align="left" :span="leftSpan">
-        <el-pagination
-          background
-          layout="total, sizes, prev, pager, next, jumper"
-          :small="small"
-          :total="total"
-          :current-page="page"
-          :page-size="size"
-          :page-sizes="sizes"
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          @prev-click="handlePrevClick"
-          @next-click="handleNextClick" />
-      </el-col>
-      <el-col align="right" :span="6" v-if="this.$slots.default">
-        <slot></slot>
-      </el-col>
-    </el-row>
-  </div>
+  <el-row type="flex" align="middle">
+    <el-col align="left" :span="leftSpan">
+      <el-pagination
+        background
+        layout="total, sizes, prev, pager, next, jumper"
+        :small="small"
+        :total="total"
+        :current-page="page"
+        :page-size="size"
+        :page-sizes="sizes"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        @prev-click="handlePrevClick"
+        @next-click="handleNextClick" />
+    </el-col>
+    <el-col align="right" :span="6" v-if="this.$slots.default">
+      <slot></slot>
+    </el-col>
+  </el-row>
 </template>
 <script>
 import PaginationMixin from './mixins/pagination'
--- packages/pagination-table/src/table.vue
+++ packages/pagination-table/src/table.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="thx-widget">
-    <el-table
-      ref="table"
-      style="width: 100%;"
-      stripe
-      border
-      fit
-      highlight-current-row
-      :data="data"
-      :height="height"
-      :max-height="maxHeight"
-      @select="handleSelect"
-      @select-all="handleSelectAll"
-      @selection-change="handleSelectionChange"
-      @row-click="handleRowClick"
-      @row-dblclick="handleRowDblclick">
-      <slot>
-        <el-table-column label="Columns" align="center"></el-table-column>
-      </slot>
+  <el-table
+    ref="table"
+    style="width: 100%;"
+    stripe
+    border
+    fit
+    highlight-current-row
+    :data="data"
+    :height="height"
+    :max-height="maxHeight"
+    @select="handleSelect"
+    @select-all="handleSelectAll"
+    @selection-change="handleSelectionChange"
+    @row-click="handleRowClick"
+    @row-dblclick="handleRowDblclick">
+    <slot>
+      <el-table-column label="Columns" align="center"></el-table-column>
+    </slot>
 
-      <template v-slot:append>
-        <slot name="append"></slot>
-      </template>
-    </el-table>
-  </div>
+    <template v-slot:append>
+      <slot name="append"></slot>
+    </template>
+  </el-table>
 </template>
 <script>
 import TableMixin from './mixins/table'
