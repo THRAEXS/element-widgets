be949993b9918ae13245ba9a38832b9a311a0350
鬼王 <THRAEX@aliyun.com>
2020-12-19 12:41:02 +0800

    chore(docs): config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,55 @@
 module.exports = {
+  // base: '/element-widgets/',
   title: 'Element Widgets',
   description: 'Widget based on element-ui',
+  port: 9718,
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' }
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Others', link: '/others/' }
     ],
     // sidebar: [
     //   // '/',
-    //   {
-    //     title: 'Table',
-    //     path: '/guide/table/',
-    //     children: [
-    //       '',
-    //       'pagintaion-table'
-    //     ]
-    //   }
+    //   // {
+    //   //   title: 'Table',
+    //   //   path: '/guide/table/',
+    //   //   children: [
+    //   //     '',
+    //   //     'pagintaion-table'
+    //   //   ]
+    //   // }
+    //   '/',
+    //   '/guide/',
+    //   '/guide/table/',
+    //   // '/guide/table/pagination/table',
     // ]
+    // sidebarDepth: 0,
     sidebar: {
       '/guide/': [
-        '',
+        // 'table/',
+        // // 'table/',
+        // // 'pagination-table'
+        // // 'o-t',
+        // // 'one',
+        // 'table/pagination-table',
+        // 'pagination/'
+        // {
+        //   title: 'Table',
+        //   path: '/guide/table/',
+        //   children: [
+        //     '/guide/table/',
+        //     '/guide/table/pagination-table'
+        //   ]
+        // }
+        // '',
         'table/',
-        'table/pagination-table'
-      ]
+        'pagination',
+        'pagination-selection'
+      ],
+      /* '/others/': [
+        ''
+      ] */
     }
   }
 }
