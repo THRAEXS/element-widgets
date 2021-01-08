7bff70e8648389f3a307d5d37578bbc11197a5a8
鬼王 <THRAEX@aliyun.com>
2021-01-08 19:01:28 +0800

    save(tree-cascader): case

--- examples/views/tree-cascader/index.vue
+++ examples/views/tree-cascader/index.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>Tree Cascader</div>
+  <div>
+    Tree Cascader
+    <div>{{ data }}</div>
+  </div>
 </template>
 <script>
 import { getOrgTree } from '@/api'
 
 export default {
   name: 'CaseTreeCascader',
+  data() {
+    return {
+      data: null
+    }
+  },
   created() {
-    getOrgTree().then(data => console.debug(data))
+    getOrgTree().then(data => (this.data = data))
   }
 }
 </script>
