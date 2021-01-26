9eb1bc52e7bb90762b2b256eacc6bf9bd1a27130
鬼王 <THRAEX@aliyun.com>
2021-01-26 11:05:28 +0800

    test: $attr/$props/$listeners

--- examples/views/tree-cascader/index.vue
+++ examples/views/tree-cascader/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     Tree Cascader
-    <div>{{ data }}</div>
+    <!-- <div>{{ data }}</div> -->
+
+    <thx-tree-cascader
+      size="mini"
+      placeholder="123"
+      clearable
+      @focus="handleFocus"
+      @blur="handleBlur"
+    />
   </div>
 </template>
 <script>
@@ -16,6 +24,14 @@ export default {
   },
   created() {
     getOrgTree().then(data => (this.data = data))
+  },
+  methods: {
+    handleFocus() {
+      console.debug('focus:', arguments)
+    },
+    handleBlur() {
+      console.debug('blur:', arguments)
+    }
   }
 }
 </script>
--- packages/tree-cascader/src/main.vue
+++ packages/tree-cascader/src/main.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>
-    Tree Cascader
-  </div>
+  <el-cascader v-bind="$attrs" v-on="$listeners" />
 </template>
 <script>
 export default {
-  name: 'ThxTreeCascader'
+  name: 'ThxTreeCascader',
+  props1: {
+    value: [String, Array],
+    options: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    props: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    /* size: {
+      type: String,
+      validator(value) {
+        return ['medium', 'small', 'mini'].indexOf(value) !== -1
+      }
+    }, */
+    size: String,
+    placeholder: String,
+    disabled: Boolean,
+    clearable: Boolean,
+    showAllLevels: {
+      type: Boolean,
+      default: true
+    },
+    collapseTags: Boolean,
+    separator: String,
+    filterable: Boolean,
+    filterMethod: Function,
+    debounce: Number,
+    beforeFilter: Function
+  },
+  mounted() {
+    console.debug('$attrs:', this.$attrs)
+    console.debug('$props:', this.$props)
+    console.debug('$listeners:', this.$listeners)
+  }
 }
 </script>
